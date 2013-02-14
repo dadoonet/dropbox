e5238553e3dafc7d2bb4084a05139089c01f7765
David Pilato <david@pilato.fr>
2013-02-14 12:39:07 +0100

    Add loggers and use maven properties for itests

--- src/test/java/org/elasticsearch/river/dropbox/itest/UserInfoTest.java
+++ src/test/java/org/elasticsearch/river/dropbox/itest/UserInfoTest.java
@@ -19,22 +19,17 @@
 
 package org.elasticsearch.river.dropbox.itest;
 
-import java.io.BufferedOutputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.Properties;
-
-import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.Test;
-
 import fr.pilato.elasticsearch.river.dropbox.connector.DropboxAccount;
 import fr.pilato.elasticsearch.river.dropbox.connector.DropboxChanges;
 import fr.pilato.elasticsearch.river.dropbox.connector.DropboxConnector;
 import fr.pilato.elasticsearch.river.dropbox.connector.DropboxFile;
+import org.apache.log4j.Logger;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.*;
+import java.util.Properties;
 
 public class UserInfoTest {
 	private static String appkey = null;
@@ -43,6 +38,7 @@ public class UserInfoTest {
 	private static String secret = null;
 	private static DropboxConnector dbConnector = null;
 
+    private static final Logger logger = Logger.getLogger(UserInfoTest.class);
 	/**
 	 * Load Dropbox properties
 	 * @throws Exception
@@ -97,27 +93,31 @@ public class UserInfoTest {
 				}
 			}
 		}
-		
-		// TODO Use a logger
-		System.out.println(changes.getCursor() + " - " + changes.getFiles().size() + " : " + 
-				nbDir + " dirs, " + nbFiles + " files, " + nbDeleted + " deleted");
+
+        logger.info("Dropbox Cursor : " + changes.getCursor());
+        logger.info("  - Number of changes " + changes.getFiles().size());
+        logger.info("  - dirs " + nbDir);
+        logger.info("  - files " + nbFiles);
+        logger.info("  - deleted " + nbDeleted);
 	}
 	
 	@Test
 	public void getFile() {
 		byte[] file = dbConnector.getFiles("dropbox", "/test-es/Premierspas.pdf");
 		Assert.assertNotNull(file);
+        logger.info("File size : " + file.length);
 		// write(file, "premierspas.pdf");
 		
 		file = dbConnector.getFiles("dropbox", "/ASL Angélique Myrtilles/Comité/CRRéunion-2011-07-04.doc");
 		Assert.assertNotNull(file);
+        logger.info("File size : " + file.length);
 		// write(file, "cr.doc");
 		
 		
 	}
 	
 	void write(byte[] aInput, String aOutputFileName){
-		System.out.println("Writing binary file...");
+        logger.info("Writing binary file...");
 	    try {
 	      OutputStream output = null;
 	      try {
@@ -129,10 +129,10 @@ public class UserInfoTest {
 	      }
 	    }
 	    catch(FileNotFoundException ex){
-	      System.err.println("File not found.");
+            logger.error("File not found.");
 	    }
 	    catch(IOException ex){
-	    	System.err.println(ex);
+            logger.error(ex);
 	    }
 	  }
 	  
